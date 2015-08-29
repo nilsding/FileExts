16da9c05b7167130a61cf17f5243319c42d5821f
Georg G <nilsding@nilsding.org>
2015-08-29 02:16:11 +0200

    Ramses will das so.

---
+++ lib/followback.rb
@@ -0,0 +1,7 @@
+Twittbot::BotPart.new :followback do
+  on :follow do |obj|
+    if obj.target.screen_name == @config[:screen_name] # only follow source if the target was the bot
+      bot.follow(obj.source.id)
+    end
+  end
+end
