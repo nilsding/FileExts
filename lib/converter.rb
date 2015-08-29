ff78446f90a9426c59e0088556c6440fd1726592
Georg G <nilsding@nilsding.org>
2015-08-29 02:13:37 +0200

    h

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,7 +1,7 @@
 Twittbot::BotPart.new :converter do
   every 30, :minutes do
     ext1 = @config[:extensions].sample
-    ext2 = @config[:extensions].sample while ext1 == ext2
+    ext2 = @config[:extensions].sample
     bot.tweet ".#{ext1} to .#{ext2} converter"
   end
 end
